7aee27e2887533d870037e1bed2bdd67b5602196
RipalPatel18 <n01578468@humber.ca>
2025-09-30 15:53:37 -0400

    Fixed Errors in js file

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
+// Get the current date
 var today = new Date();
+
 
 console.log("Current date and time:", today);
 
 
-var hour = today.getHours();
-var day = today.getDay();
+var hour = today.getHours(); 
+var day = today.getDay();   
+
+
+document.write("<p>Day of the week: " + day + "</p>");
+document.write("<p>Hour of the day: " + hour + "</p>");
+
 var greeting = "";
 
-
-if (day === 1) {
-    greeting = "Do you have a case of the Mondays?";
-}
-
-else if (day === 5 && hour >= 12) {
-    greeting = "It’s almost the weekend!";
-}
-
-else if (hour < 12) {
-    greeting = "Have a good morning!";
-}
-
-else if (day === 0 || day === 6) {
+// Determine the appropriate greeting
+if (day === 0 || day === 6) {
+    // Weekend (Saturday = 6, Sunday = 0)
     greeting = "It’s the weekend!!!";
-}
-
-
-else {
+} else if (day === 1) {
+    // Monday
+    greeting = "Do you have a case of the Mondays?";
+} else if (day === 5 && hour >= 12) {
+    // Friday afternoon
+    greeting = "It’s almost the weekend!";
+} else if (hour < 12) {
+    // Before noon
+    greeting = "Have a good morning!";
+} else {
+    // Any other afternoon on a weekday
     greeting = "Have a good day!";
 }
+
+
+document.getElementById("greeting-message").innerText = greeting;
